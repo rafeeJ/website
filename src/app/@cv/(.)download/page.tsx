f1375bc037685588d3c4b2bd1b703fac07a08f49
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-02-06 23:18:25 +0000

    feat: adds copy

--- src/app/@cv/(.)download/page.tsx
+++ src/app/@cv/(.)download/page.tsx
@@ -2,11 +2,13 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useRouter } from "next/navigation";
 import { DownloadCvButton } from "@/features/cv-download/download-cv-button";
+import { InfoIcon } from "lucide-react";
 
 export default function Download() {
   const router = useRouter();
@@ -20,6 +22,12 @@ export default function Download() {
         <DialogContent>
           <DialogHeader>
             <DialogTitle>thanks for looking!</DialogTitle>
+            <DialogDescription>
+              <p>
+                the button below will download my CV as a pdf using the Google
+                Docs API.
+              </p>
+            </DialogDescription>
           </DialogHeader>
           <DownloadCvButton />
         </DialogContent>
